516b643590593674c6762ec52007f313a911d854
Jannik Arndt <jannik@jannikarndt.de>
2016-10-30 15:17:38 +0100

    Added exit-on-error and commit-message-reading to deploy-script.
    Also, since building does not need a template anymore, so it’s just
    “hugo”, there’s no need for a build script anymore.

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# build project
-./build.sh
+echo "\033[2m"
+# Build the project.
+hugooutput="$(hugo)"
+echo "$hugooutput"
+echo "\033[0m"
+
+if echo "$hugooutput" | grep "ERROR"
+  then echo "\033[31mError during build, cancelling deployment. 🙁\033[0m"
+  exit
+fi
 
 # Add changes to git.
 git add -A
 
 # Commit changes.
-msg="rebuilding site `date`"
 if [ $# -eq 1 ]
-  then msg="$1"
+    then msg="$1"
+else
+    echo "\033[1mCommit message: "
+    read newmsg
+    echo "\033[0m"
+    msg=newmsg
 fi
+
 git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
 git subtree push --prefix=public git@github.com:NiceToKnow/IT.git gh-pages
+echo "New version is deployed 👍"
